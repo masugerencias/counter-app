60e9803ef5db8899e615eb1e66b810d20357b98a
masugerencias <yannpoirotsanz@gmail.com>
2022-09-08 20:56:44 +0200

    counter working

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,26 +1,30 @@
 
-
+import { useState } from 'react'
 import React from 'react'
 import PropTypes from 'prop-types'
 
-/* hanndle add */
-const handdleAdd = (e) => {
-    console.log('otro masss')
-}
 
 
+function CounterApp({ value }) {
 
-function CounterApp({value}) {
+
+  const [counter, setCounter] = useState(0)
+  console.log(counter)
+
+  /* hanndle add */
+  const handdleAdd = () => {
+    setCounter (counter +1)
+  }
   return (
     <div>
-    <h2>Este es el valor del prop que he pasado del padre : {value}</h2>
-<button onClick={ handdleAdd }>+1</button>
+      <h2>Este es el valor del prop que he pasado del padre : {counter}</h2>
+      <button onClick={handdleAdd}>+1</button>
     </div>
   )
 }
 
 CounterApp.propTypes = {
-    value: PropTypes.number
+  value: PropTypes.number
 }
 
 export default CounterApp

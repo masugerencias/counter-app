686c9c8a207849009a5672f3e0a2bb0ae1ceb2ca
masugerencias <yannpoirotsanz@gmail.com>
2022-09-08 21:09:09 +0200

    section ONE accomplished

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -8,17 +8,35 @@ import PropTypes from 'prop-types'
 function CounterApp({ value }) {
 
 
-  const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(value)
   console.log(counter)
 
   /* hanndle add */
-  const handdleAdd = () => {
+  const handlerAdd = () => {
     setCounter (counter +1)
   }
+
+  /* handler reset */
+  const handlerReset = () => {
+    setCounter (value)
+  }
+
+
+  /* handler substract */
+const handlerSubstract = () => {
+  setCounter (counter -1 )
+}
+
+
+
   return (
     <div>
       <h2>Este es el valor del prop que he pasado del padre : {counter}</h2>
-      <button onClick={handdleAdd}>+1</button>
+      <button onClick={handlerAdd}>+1</button>
+      <button onClick={handlerReset}>Reset</button>
+      <button onClick={handlerSubstract}>-1</button>
+      {/* los handlers se podrían haber puesto como funciones en linea dentro de los botones */}
+
     </div>
   )
 }
